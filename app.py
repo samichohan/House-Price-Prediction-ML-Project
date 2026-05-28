0a6d5a385b08d7f92072ff5dc87935716ae7b225
samichohan <samichohan525@gmail.com>
2026-05-28 14:53:53 +0500

    fix model path

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ st.set_page_config(
 # ─── Load Model ────────────────────────────────────────────
 @st.cache_resource
 def load_model():
-    return joblib.load("house_model.pkl")
+    return joblib.load("model/house_model.pkl")
 
 model = load_model()
 
